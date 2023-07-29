499611d5460df294978c16c39e0f0b83c54631ec
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-07-29 15:25:12 -0700

    starting tests

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
-// const v1Routes = require('./routes/v1.js');
 const v2Routes = require('./routes/v2.js');
 const logger = require('./middleware/logger.js');
 
@@ -23,8 +22,7 @@ app.use(logger);
 
 // Routes
 app.use(authRoutes);
-// app.use('/api/v1', v1Routes);
-app.use('/api/v2', v2Routes);
+app.use('/', v2Routes);
 
 // Catchalls
 app.use(notFound);
